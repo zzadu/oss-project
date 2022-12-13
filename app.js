5523b4376891759996992071d3adefb273d15f90
zzadu <zzadu08@naver.com>
2022-12-14 02:26:33 +0900

    edit app.js

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const PORT = 80;
 
 const app = express();
 app.get('/', function(req, res) {
-    res.sendFile('/index.html');
+    res.sendFile('index.html');
 });
 
 app.listen(PORT);
-console.log('Running on http://localhost:' + PORT);
+console.log('Running on PORT ' + PORT);

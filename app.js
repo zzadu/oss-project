7e9cdaa388d731ff3ebc793fa5fd61651375e064
zzadu <zzadu08@naver.com>
2022-12-14 15:03:05 +0900

    edit app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const PORT = process.env.PORT || 80
 
 const app = express();
-app.use('/Build', express.static(path.join(__dirname, 'Build')));
+app.use('/Build', express.static(__dirname +'/Build'));
 app.get('/', function(req, res) {
     res.sendFile(__dirname + "/index.html")
 })
